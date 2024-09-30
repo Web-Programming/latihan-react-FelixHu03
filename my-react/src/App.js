ad7adfd5bc2729bca9a86ebd05942bd887eb03ee
Felix <felix_2327250059@mhs.mdp.ac.id>
2024-09-30 20:35:12 +0700

    masih salah

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,23 +1,54 @@
 // import logo from './logo.svg';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Hello from './Hello';
 import Product from './Product';
 import Registrasi from './Registrasi';
+import React from 'react';
+import routes from './route';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Hello
-        name = "Felix"
-        pesan = "Selamat datang di kelas IF-31"
-       />
-       <Hello/>
-       <Product/>
-       <Registrasi/>
-      </header>
-    </div>
+    <React.Suspense fallback={<div>Loading ....</div>}>
+      <div className='App'>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <NavLink to="/" className={"Navbar-brand" }>
+          Navbar
+        </NavLink>
+        <button className='navbar-toggler' type=''>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id='navbarNav'>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/product" className="nav-link">Product</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/register" className="nav-link">Register</NavLink>
+        </li>
+        
+      </ul>
+      </div>
+        
+      </nav>
 
+        <div className='main'>
+        <Routes>
+            {
+              routes.map((route, i)=>{
+                const {path, Component} = route;
+                return <Route key ={i} path = {path} element = {<Component />} />;
+              })
+            }
+          </Routes>
+        </div>
+      </div>
+    </React.Suspense>
   );
 }
 
