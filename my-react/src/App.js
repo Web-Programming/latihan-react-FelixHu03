3322c36a0b6c85f84c55daf18261bb35177e9500
Felix <felix_2327250059@mhs.mdp.ac.id>
2024-10-03 19:11:10 +0700

    membuat dengan menggunakan dom

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -11,38 +11,38 @@ function App() {
   return (
     <React.Suspense fallback={<div>Loading ....</div>}>
       <div className='App'>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container">
-        <NavLink to="/" className={"Navbar-brand" }>
-          Navbar
-        </NavLink>
-        <button className='navbar-toggler' type=''>
-          <span className='navbar-toggler-icon'></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse" id='navbarNav'>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink to="/product" className="nav-link">Product</NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink to="/register" className="nav-link">Register</NavLink>
-        </li>
-        
-      </ul>
-      </div>
-        
-      </nav>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <NavLink to="/" className="Navbar-brand">
+              Navbar
+            </NavLink>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse" id='navbarNav'>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink to="/product" className="nav-link">Product</NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink to="/register" className="nav-link">Register</NavLink>
+              </li>
+
+            </ul>
+          </div>
+
+        </nav>
 
         <div className='main'>
-        <Routes>
+          <Routes>
             {
-              routes.map((route, i)=>{
-                const {path, Component} = route;
-                return <Route key ={i} path = {path} element = {<Component />} />;
+              routes.map((route, i) => {
+                const { path, Component } = route;
+                return <Route key={i} path={path} element={<Component />} />;
               })
             }
           </Routes>
